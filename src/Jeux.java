272f169e3223ecbff82dcd10a0a245bb49f8fa8a
blancgr <gregory.blanc@iut-valence.fr>
2014-02-17 17:47:27 +0100

    Initial commit.

---
+++ src/Jeux.java
@@ -0,0 +1,31 @@
+
+public class Jeux 
+{
+	private static final int PION_JOUEUR_1 = 1;
+	private static final int PION_JOUEUR_2 = 2;
+	
+	public static int tableau[][] = {
+		 			{0,0,0,0,0,0,0},
+		 			{0,0,0,0,0,0,0},
+		 			{0,0,0,0,0,0,0},
+		 			{0,0,0,0,0,0,0},
+		 			{0,0,0,0,0,0,0},
+		 			{0,0,0,0,0,0,0},
+ 					};
+
+	public void joueur1()
+{
+	tableau[6][7] = PION_JOUEUR_1;
+}
+
+	public void joueur2()
+{
+	tableau[6][7] = PION_JOUEUR_2;
+}
+
+public void joueurCourant(joueur);
+{
+	joueur = joueur1;
+}
+System.out.println(tableau
+}
