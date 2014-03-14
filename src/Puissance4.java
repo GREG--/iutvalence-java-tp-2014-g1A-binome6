b6237b5e3837db11fa3e8fd9478341cf21fa9f0f
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:59:00 +0100

    Remove all the old codes.
    Use this commit to retrieve it.

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,53 +1,17 @@
+/* TODO Package. */
+/* TODO Javadoc. */
 /**
  * @Author : Blanc-Partula
  */
-public class Puissance4 
-{
-	public Etat joueurCourant;
-	public Grille grille;
+public class Puissance4 {
+    public Etat   joueurCourant;
+    public Grille grille;
 
-	
-	public void jouer() {
-    	System.out.println(new Grille());
-    	/* Affiche mettre dans application*/
+    public void jouer() {
+        System.out.println(new Grille());
+        /* Affiche mettre dans application*/
 		/* Saisir mettre dans application*/
 		/* Tenter de poser. */
 		/* Victoire ? */
-	}
-	
-	//public final static int COLONNE_PLEINE = -1;
-	//public final static int SUCCES = -2;
-	//private final int capacite;
-	//private int taille;
-	//private final int[] elements;
-	
-
-//	public void joueur1()
-//{
-//	tableau[6][7] = PION_JOUEUR_1;
-//}
-//	public void joueur2()
-//{
-//	tableau[6][7] = PION_JOUEUR_2;
-//}
-//	public void joueurCourant();
-//	{
-//	joueur = joueur1;
-//	}
-//	
-//	public void placerPion()
-//	{
-//	private int numColonne;
-//	}
-//	
-	/** Méthode à finaliser */
-	//public int empiler(int pionAEmpiler)
-	//{
-	//this.capacite=6;
-	//if (this.taille == this.capacite)
-	//return COLONNE_PLEINE;
-	//this.elements[this.taille] = pionAEmpiler;
-	//this.taille = this.taille + 1;
-	//return SUCCES;
-	//}
+    }
 }
