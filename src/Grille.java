6ec62b61add5ca3c7557af56fd33904c11833890
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 12:02:54 +0100

    Add TODO (Grille.java).

--- src/Grille.java
+++ src/Grille.java
@@ -1,54 +1,48 @@
-public class Grille
-{
-/**
-* Le nombre de cases par ligne
-*/
-private static final int NOMBRE_DE_LIGNES = 6;
+/* TODO Package. */
+/* TODO Javadoc. */
+/* TODO J'ai corrig√© l'indentation. Faites-le ! */
+public class Grille {
+    /** Le nombre de cases par ligne. */
+    private static final int NOMBRE_DE_LIGNES = 6;
 
-/**
-* Le nombre de cases par colonne
-*/
-private static final int NOMBRE_DE_COLONNES = 7;
+    /** Le nombre de cases par colonne. */
+    private static final int NOMBRE_DE_COLONNES = 7;
 
-/**
-* Les cases de la grille
-*/
-private Etat[][] cases;
+    /* TODO Change en cours de partie ? */
+    /** Les cases de la grille. */
+    private Etat[][] cases;
 
-/**
-* Construire une nouvelle grille, vide
-*/
-public Grille()
-{
-this.cases = new Etat[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
+    /** Construire une nouvelle grille vide. */
+    public Grille() {
+        this.cases = new Etat[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
 
-for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-	for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-		this.cases[numeroDeLigne][numeroDeColonne] = Etat.VIDE;
-}
+        for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++) {
+            for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++) {
+                this.cases[numeroDeLigne][numeroDeColonne] = Etat.VIDE;
+            }
+        }
+    }
 
-/**
-* Obtenir une reprÈsentation en ASCII-art de la grille, o˘ les cases
-* sont reprÈsentÈes par des O
-*
-* @see java.lang.Object#toString()
-*/
-public String toString()
-{
-String grillealpha = "";
+    /* TODO Peut-√™tre simplifi√© par un usage pertinent des "enum" */
+    /* TODO Mauvais comportement. Vous ne distinguez pas les joueurs. */
+    /**
+     * Obtenir une repr√©sentation en ASCII-art de la grille.
+     * <p/>
+     * Les cases sont repr√©sent√©es par des O.
+     */
+    public String toString() {
+        String grillealpha = "";
 
-for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-{
-for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-{
-if (this.cases[numeroDeLigne][numeroDeColonne] == Etat.VIDE)
-grillealpha += "O ";
-else
-	grillealpha += "R ";
-}
-grillealpha += "\n";
-}
+        for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++) {
+            for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++) {
+                if (this.cases[numeroDeLigne][numeroDeColonne] == Etat.VIDE) {
+                    grillealpha += "O ";
+                }
+                else { grillealpha += "R "; }
+            }
+            grillealpha += "\n";
+        }
 
-return grillealpha;
-}
+        return grillealpha;
+    }
 }
