5fc9bd5ef06baf199ecaa5a0b30dbe9f8482f2c7
partulaj99 <jeremie.partula@iut-valence.fr>
2014-04-02 22:59:20 +0200

    02/04/2014 Implémentation des tour de jeu

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,4 +1,5 @@
 /* TODO Package. */
+import java.util.Scanner;
 
 /**
  * TODO.
@@ -8,25 +9,37 @@
  */
 public class Puissance4 {
 	/** TODO. */
+
 	public Etat joueurCourant;
 	/* TODO Change en cours de partie ? */
 	/** TODO. */
 	public Grille grille;
 
-	
 	public Puissance4() {
 		this.joueurCourant = Etat.JOUEUR_1;
 		this.grille = new Grille();
 	}
 
-
 	public void jouer() {
-		System.out.println(grille);
-		/* Saisir */
-		/* Tenter de poser. */
-		/* Victoire. */
-		/* Saisir mettre dans application */
-		/* Tenter de poser. */
-		/* Victoire ? */
+		int i = 0;
+		while (i <= 0) {
+			System.out.println("Veuillez saisir le numéro de colonne");
+			Scanner sc = new Scanner(System.in);
+			int numScan = sc.nextInt();
+			grille.placerPion(numScan, joueurCourant);
+			System.out.println(grille);
+			if (joueurCourant == Etat.JOUEUR_1) {
+				joueurCourant = Etat.JOUEUR_2;
+			} else {
+				joueurCourant = Etat.JOUEUR_1;
+			}
+			i = i - 1;
+			/* Saisir */
+			/* Tenter de poser. */
+			/* Victoire. */
+			/* Saisir mettre dans application */
+			/* Tenter de poser. */
+			/* Victoire ? */
+		}
 	}
 }
